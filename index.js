125ae9920fc8abb9cfd0b31b14d56bec6d81f1cb
Misbahul Arifin <33975227+msbarf14@users.noreply.github.com>
2022-07-03 10:02:39 +0800

    update enpoint

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
 
-const url = "https://tmial-amien.sch.id/category/berita/"
-
+const url = "https://tmial-amien.sch.id/"
 const app = express()
 
 app.get('/', (req, res) => {
@@ -13,29 +12,47 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
-    axios.get(url)
+    axios.get(url+'category/berita/')
     .then(response => {
         const html = response.data
         const news = []
         const $ = cheerio.load(html)
         $('.post-item', html).each(function() {
             const title = $(this).find('.post-title').text()
+            const desc = $(this).find('.from_the_blog_excerpt ').text()
             const link = $(this).find('a').attr('href')
             const img = $(this).find('img').attr('src')
             news.push({
                 img,
                 title,
+                desc,
                 link
             })
         })
-
         res.json(news)
     })
 })
 
-
-
-
-
+app.get('/news/kolom-pengasuh', (req, res) => {
+    axios.get(url+'category/kolom-pengasuh/')
+    .then(response => {
+        const html = response.data
+        const news = []
+        const $ = cheerio.load(html)
+        $('.post-item', html).each(function() {
+            const title = $(this).find('.post-title').text()
+            const desc = $(this).find('.from_the_blog_excerpt ').text()
+            const link = $(this).find('a').attr('href')
+            const img = $(this).find('img').attr('src')
+            news.push({
+                img,
+                title,
+                desc,
+                link
+            })
+        })
+        res.json(news)
+    })
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
